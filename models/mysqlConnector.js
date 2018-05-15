58121b151fc9b85be50309885afdbe770a5fd843
NgocQuynh1206 <36333470+NgocQuynh1206@users.noreply.github.com>
2018-05-15 23:09:03 +0700

    Update mysqlConnector.js

--- models/mysqlConnector.js
+++ models/mysqlConnector.js
@@ -2,9 +2,10 @@
 var mysql = require('mysql');
 // Initialize pool
 var pool      =    mysql.createPool({
-    host     : '127.0.0.1',
-    user     : 'root',
-    database : 'sportshop',
+    host     : 'us-cdbr-iron-east-04.cleardb.net',
+    user     : 'b97c46713bc01e',
+    password : '4e6cb8ff'
+    database : 'heroku_94045600a237def',
     debug    :  false
 });
 module.exports = pool;
