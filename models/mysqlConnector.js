3e55726f2ae90964e7b1875f8a67612e7e1730a7
AzureAD\PHANNGUYENNGOCQUYNH <johndoe@example.com>
2018-05-15 23:27:21 +0700

    my commit

--- models/mysqlConnector.js
+++ models/mysqlConnector.js
@@ -2,10 +2,9 @@
 var mysql = require('mysql');
 // Initialize pool
 var pool      =    mysql.createPool({
-    host     : 'us-cdbr-iron-east-04.cleardb.net',
-    user     : 'b97c46713bc01e',
-    password : '4e6cb8ff'
-    database : 'heroku_94045600a237def',
+    host     : '127.0.0.1',
+    user     : 'root',
+    database : 'sportshop',
     debug    :  false
 });
 module.exports = pool;
