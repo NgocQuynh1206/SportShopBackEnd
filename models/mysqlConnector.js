a9ac49db145a4caaef6b503725c49e31e9254c37
NgocQuynh1206 <36333470+NgocQuynh1206@users.noreply.github.com>
2018-05-22 23:59:14 +0700

    Update mysqlConnector.js

--- models/mysqlConnector.js
+++ models/mysqlConnector.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 var pool      =    mysql.createPool({
     host     : 'us-cdbr-iron-east-04.cleardb.net',
     user     : 'b97c46713bc01e',
-    password : '4e6cb8ff'
+    password : '4e6cb8ff',
     database : 'heroku_94045600a237def',
     debug    :  false
 });
